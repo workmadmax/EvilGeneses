bc93a5b373d3f2b6fa6e57468f2cf1ed9d533587
workmadmax <80357524+workmadmax@users.noreply.github.com>
2021-10-14 21:08:09 -0300

    Att Game

--- src/script.js
+++ src/script.js
@@ -1,88 +1,68 @@
 const _data = {
-	gameOn: false,
-	timeout: undefined,
-	sounds: [],
+  gameOn: false,
+  timeout: undefined,
+  sounds: [],
 
-	strict: false,
-	playerCanPlay: false,
-	score: 0,
-	gameSequence: [],
-	playerSequence: []
+  strict: false,
+  playerCanPlay: false,
+  score: 0,
+  gameSequence: [],
+  playerSequence: [],
 };
 
 const _gui = {
-	counter: document.querySelector(".gui__counter"),
-	switch: document.querySelector(".gui__btn-switch"),
-	led: document.querySelector(".gui__led"),
-	strict: document.querySelector(".gui__btn--strict"),
-	start: document.querySelector(".gui__btn--start"),
-	pads: document.querySelectorAll(".game__pad")
-}
+  counter: document.querySelector(".gui__counter"),
+  switch: document.querySelector(".gui__btn-switch"),
+  led: document.querySelector(".gui__led"),
+  strict: document.querySelector(".gui__btn--strict"),
+  start: document.querySelector(".gui__btn--start"),
+  pads: document.querySelectorAll(".game__pad"),
+};
 
 const _soundUrls = [
-	"audio/simonSound1.mp3",
-	"audio/simonSound2.mp3",
-	"audio/simonSound3.mp3",
-	"audio/simonSound4.mp3"
+  "../assets/audio/audio_simonSound1.mp3",
+  "../assets/audio/audio_simonSound2.mp3",
+  "../assets/audio/audio_simonSound3.mp3",
+  "../assets/audio/audio_simonSound4.mp3",
 ];
 
-_soundUrls.forEach(sndPath => {
-	const audio = new Audio(sndPath);
-	_data.sounds.push(audio);
+_soundUrls.forEach((sndPath) => {
+  const audio = new Audio(sndPath);
+  _data.sounds.push(audio);
 });
 
 _gui.switch.addEventListener("click", () => {
+  _data.gameOn = _gui.switch.classList.toggle("gui__btn-switch--on");
+  console.log(_data.gameOn)
 
+  _gui.counter.classList.toggle("gui__counter--on");
+  _gui.counter.innerHTML = "--";
 });
 
-_gui.strict.addEventListener("click", () => {
+_gui.strict.addEventListener("click", () => {});
 
+_gui.start.addEventListener("click", () => {});
+
+const padListener = (e) => {};
+
+_gui.pads.forEach((pad) => {
+  pad.addEventListener("click", padListener);
 });
 
-_gui.start.addEventListener("click", () => {
+const startGame = () => {};
 
-});
+const setScore = () => {};
 
-const padListener = (e) => {
+const newColor = () => {};
 
-}
+const playSequence = () => {};
 
-_gui.pads.forEach(pad => {
-	pad.addEventListener("click", padListener);
-});
+const blink = (text, callback) => {};
 
-const startGame = () => {
+const waitForPlayerClick = () => {};
 
-}
+const resetOrPlayAgain = () => {};
 
-const setScore = () => {
+const changePadCursor = (cursorType) => {};
 
-}
-
-const newColor = () => {
-
-}
-
-const playSequence = () => {
-
-}
-
-const blink = (text, callback) => {
-
-}
-
-const waitForPlayerClick = () => {
-
-}
-
-const resetOrPlayAgain = () => {
-
-}
-
-const changePadCursor = (cursorType) => {
-
-}
-
-const disablePads = () => {
-
-}
+const disablePads = () => {};
